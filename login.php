e359898984e627e523801c4da1e9be7c50449766
AkosiHakdogkoMalaki <73035780+AkosiHakdogkoMalaki@users.noreply.github.com>
2020-11-02 15:58:27 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,18 +2,35 @@
     include_once 'header.php';
 ?>
 
-<section>
-    <h2>Login</h2>
-    <form action="includes/login.inc.php" method="post">
-        <label for="uname">Username:</label>
-        <input type="text" name="uname" placeholder="Username..."> 
-        <br><br>
-        <label for="pword">Password</label>
-        <input type="password" name="pword" placeholder="Password...">
-        <br><br>
-        <button type="submit" name="login">Login</button>
-    </form>
-</section>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COVID19 Status Tracking System</title>
+    <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="css/login.css"/>
+</head>
+<body>
+    <div class="login-div">
+        <div class="logo"><img src="img/logo.png" alt=""></div>
+            <div class="title">Covid-19 Status Tracking System</div>
+            <div class="sub-title">Login</div>
+                <form action="">
+                    <div class="fields">
+                        <div class="username">
+                            <input type="username" class="user-input" placeholder="username" />
+                        </div>
+                        <div class="password">
+                            <input type="password" class="pass-input" placeholder="password" />
+                        </div>
+                    </div>
+                        <button class="signin-button">Login</button>
+                </form>
+        <div class="link">
+            <a href="#">Forgot password?</a> or <a href="#">Sign up</a>
+        </div>
+    </div>
+
+</body>
 
 <?php
     if(isset($_GET['login'])) {
