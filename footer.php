a9c74018d07e0d6de32dc632d2b2a99df0e10c51
Laurence Cortez <laurencecortez26.lc@gmail.com>
2020-11-05 22:48:30 +0800

    Fixed JQuery import and added .gitignore file

--- footer.php
+++ footer.php
@@ -2,6 +2,5 @@
 </body>
 </html>
 
-<script src="./bootstrap-4.4.1-dist/jquery/jquery.min.js"></script>
-<script src="./bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+<script src="./jquery/jquery.min.js"></script>
 <script src="./bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
