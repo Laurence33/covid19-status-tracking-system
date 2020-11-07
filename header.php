d64f3bd7cf0a1eb5d6745a383fffd3f8d0f9173d
AkosiHakdogkoMalaki <73035780+AkosiHakdogkoMalaki@users.noreply.github.com>
2020-11-08 02:44:37 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>COVID19 Status Tracking System</title>
-    <link rel="stylesheet" href="./bootstrap-4.4.1-dist/css/bootstrap.min.css">
-    
-</head>
-<body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/header.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  </head>
+  <body>
+    <nav>
+      <ul>
+<li class="logo"><img id="cmslogo" src="cstslogo.png" alt=""></li>
+<li class="btn"><span class="fas fa-bars"></span></li>
+<div class="items">
+<li><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Contact</a></li>
+</div>
+<li class="search-icon">
+          <input type="search" placeholder="Search">
+          <label class="icon">
+            <span class="fas fa-search"></span>
+          </label>
+        </li>
+</ul>
+
+<script>
+      $('nav ul li.btn span').click(function(){
+        $('nav ul div.items').toggleClass("show");
+        $('nav ul li.btn span').toggleClass("show");
+      });
+    </script>
+
+  </body>
+</html>
