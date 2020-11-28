e6d3caf261c4e6675b0de51a0bf120de62d8a51b
Laurence Cortez <laurencecortez26.lc@gmail.com>
2020-11-28 12:51:00 +0800

    added some functions

--- admin/process/admin-functions.inc.php
+++ admin/process/admin-functions.inc.php
@@ -1,12 +1,14 @@
 <?php
 
 function addCase($conn, $prov_code, $citymun_code, $brgy_code, $status, $fname, $lname, $age) {
+    
+    mysqli_autocommit($conn, false); // turn off auto commit for transaction
+
     $sql = "INSERT INTO `tbl_case` (status, brgy_code, citymun_code, prov_code, fname, lname, age) VALUES ('$status', '$brgy_code', '$citymun_code', '$prov_code', '$fname', '$lname', $age);";
     if(!$result = mysqli_query($conn, $sql)) {
         //error
         exit(mysqli_error($conn));
     }
-
     return $result;
 }
 
@@ -46,6 +48,33 @@ function deleteRecord($conn, $id, $type) {
     }
 
     $sql = "DELETE FROM $table WHERE id = $id;";
+    if(!$result = mysqli_query($conn, $sql)) {
+        //error
+        exit(mysqli_error($conn));
+    }
+    return $result;
+}
+
+function getRegionCode($conn, $reg_code) {
+    $sql = "SELECT reg_code FROM tbl_province WHERE reg_code = $reg_code;";
+    if(!$result = mysqli_query($conn, $sql)) {
+        //error
+        exit(mysqli_error($conn));
+    }
+    return $result;
+}
+
+function getCaseStatus($conn, $id) {
+    $sql = "SELECT status FROM tbl_case WHERE id = $id;";
+    if(!$result = mysqli_query($conn, $sql)) {
+        //error
+        exit(mysqli_error($conn));
+    }
+    return $result;
+}
+
+function getSurStatus($conn, $id) {
+    $sql = "SELECT status FROM tbl_sur WHERE id = $id;";
     if(!$result = mysqli_query($conn, $sql)) {
         //error
         exit(mysqli_error($conn));
